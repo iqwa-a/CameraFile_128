35f6c5f395151d31aa6b9ccda22ad3b036c598f4
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 19:19:35 +0700

    HomePageBc dengan fitur inisialisasi kamera dan tampilan gambar

--- lib/home_page_bc.dart
+++ lib/home_page_bc.dart
@@ -21,7 +21,68 @@ class HomePageBc extends StatelessWidget {
               context.read<CameraBloc>().add(ClearSnackbar());
             }
           },
-          builder: (context, state) {},
+           builder: (context, state) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton.icon(
+                        icon: const Icon(Icons.camera),
+                        label: const Text('Ambil Foto'),
+                        onPressed: () {
+                          final bloc = context.read<CameraBloc>();
+                          if (bloc.state is! CameraReady) {
+                            bloc.add(InitializeCamera());
+                          }
+                          bloc.add(OpenCameraAndCapture(context));
+                        },
+                      ),
+                    ),
+                    ElevatedButton.icon(
+                      icon: Icon (Icons.folder),
+                      label: const Text('Pilih dari Galeri'),
+                      onPressed: () => context
+                          .read<CameraBloc>()
+                          .add(PickImageFromGallery()),
+                    ), 
+                  ],
+                ),
+                const SizedBox(height: 20),
+                BlocBuilder<CameraBloc, CameraState>(
+                  builder: (context, state) {
+                    final ImageFile = 
+                        state is CameraReady ? state.imageFile : null;
+
+                    return ImageFile != null
+                        ? Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Image.file(
+                                ImageFile,
+                                width: double.infinity,
+                              ),
+                            ),
+                            Text('Gambar disimpan di: ${ImageFile.path}'),
+                            ElevatedButton.icon(
+                              icon: Icon(Icons.delete),
+                              label: const Text('Hapus Gambar'),
+                              onPressed: () => context
+                                    .read<CameraBloc>()
+                                    .add(DeleteImage()),
+                            )
+                          ],
+                        )
+                      : const Padding(
+                        padding: EdgeInsets.all(12.0),
+                        child: Text('Belum ada gambar yang diambil/dipilih'),
+                      );
+                  })
+              ],
+            );
+          },
         ),
       ),
     );
