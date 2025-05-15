6d3766a2c4e35e513f1518bb4550458a78cad407
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 17:10:34 +0700

    menambahkan kelas CameraBloc untuk pengelolaan logika kamera

---
+++ lib/bloc/camera_bloc.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'package:bloc/bloc.dart';
+import 'package:camera/camera.dart';
+import 'package:materi_camera/bloc/camera_event.dart';
+import 'package:materi_camera/bloc/camera_state.dart';
+
+
+class CameraBloc extends Bloc<CameraEvent, CameraState> {
+  late final List<CameraDescription> _cameras;
+
+  CameraBloc() : super(CameraInitial()) {
+    on<InitializeCamera>(_onInit);
+    on<SwitchCamera>(_onSwitch);
+    on<ToogleFlash>(_onToggleFlash);
+    on<TakePicture>(_onTakePicture);
+    on<TapToFocus>(_onTapFocus);
+    on<PickImageFromGallery>(_onPickGallery);
+    on<OpenCameraAndCapture>(_onOpenCamera);
+    on<DeleteImage>(_onDeleteImage);
+    on<ClearSnackbar>(_onClearSnackbar);
+    on<RequestPermission>(_onRequestPermissions);
+  }
+}
