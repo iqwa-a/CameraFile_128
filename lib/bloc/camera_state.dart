293db8d1a24180e5dc015f609e0d10c7ab65bbfb
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 15:29:53 +0700

    tambah kelas state untuk pengelolaan status kamera

---
+++ lib/bloc/camera_state.dart
@@ -0,0 +1,21 @@
+part of 'camera_bloc.dart';
+
+sealed class CameraState {}
+
+final class CameraInitial extends CameraState {}
+
+final class CameraReady extends CameraState {
+  final CameraController controller;
+  final int selectedIndex;
+  final FlashMode flashMode;
+  final File? imageFile;
+  final String? snackBarMessage;
+
+  CameraReady({
+    required this.controller,
+    required this.selectedIndex,
+    required this.flashMode,
+    required this.imageFile,
+    required this.snackBarMessage,
+  });
+}
