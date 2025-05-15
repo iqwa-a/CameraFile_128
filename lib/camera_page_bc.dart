9d029517fec18501d60dac9c4c96bb1ac2fd719e
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 18:26:20 +0700

    Tambah kelas CameraPageBc dan implementasi awal untuk pengelolaan kamera

---
+++ lib/camera_page_bc.dart
@@ -0,0 +1,52 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:materi_camera/bloc/camera_bloc.dart';
+import 'package:materi_camera/bloc/camera_event.dart';
+import 'package:materi_camera/bloc/camera_state.dart';
+
+class CameraPageBc extends StatefulWidget {
+  const CameraPageBc({super.key});
+
+  @override
+  State<CameraPageBc> createState() => _CameraPageBcState();
+}
+
+class _CameraPageBcState extends State<CameraPageBc> {
+  @override
+  void initState() {
+    super.initState();
+    final bloc = context.read<CameraBloc>();
+    if (bloc.state is! CameraReady) {
+      bloc.add(InitializeCamera());
+    }
+  }
+
+  IconData _flashIcon(FlashMode mode) {
+    return switch (mode) {
+      FlashMode.auto => Icons.flash_auto,
+      FlashMode.always => Icons.flash_on,
+      _ => Icons.flash_off,
+    };
+  }
+
+  Widget _circleButton(IconData icon, VoidCallback onTap) {
+    return ClipOval(
+      child: Material(
+        color: Colors.white24,
+        child: InkWell(
+          onTap: onTap,
+          child: SizedBox(
+            width: 50,
+            height: 50,
+            child: Icon(icon, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
