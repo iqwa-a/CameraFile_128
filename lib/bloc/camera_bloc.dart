dcc5eb2f368767d9204e7534eae08e12924ae00e
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 17:19:18 +0700

    menambahkan  _onDeleteImage untuk menangani penghapusan gambar dalam CameraBloc

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -112,4 +112,17 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
       ));
     }
   }
+  Future<void> _onDeleteImage(
+      DeleteImage event, Emitter<CameraState> emit) async {
+    if (state is! CameraReady) return;
+    final s = state as CameraReady;
+    await s.imageFile?.delete();
+    emit(CameraReady(
+      controller: s.controller, 
+      selectedIndex: s.selectedIndex, 
+      flashMode: s.flashMode,
+      imageFile: null,
+      snackBarMessage: 'Gambar dihapus',
+    ));      
+  }
 }
