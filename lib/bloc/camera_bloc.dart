2d87c0a60b9ccc6633d5a54f44a529b11d75d890
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-05-15 17:12:12 +0700

    menambahkan method _onToggleFlash untuk mengubah mode flash dalam CameraBloc

--- lib/bloc/camera_bloc.dart
+++ lib/bloc/camera_bloc.dart
@@ -33,4 +33,17 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     final next = (s.selectedIndex + 1) % _cameras.length;
     await _setupController(next, emit, previous: s);
   }
+
+  Future<void> _onToggleFlash(
+    ToogleFlash event, Emitter<CameraState> emit) async {
+      if (state is! CameraReady) return;
+      final s = state as CameraReady;
+      final next = s.flashMode == FlashMode.off
+          ? FlashMode.auto
+          : s.flashMode == FlashMode.auto
+              ? FlashMode.always
+              : FlashMode.off;
+      await s.controller.setFlashMode(next);
+      emit(s.copyWith(flashMode: next));
+    }
 }
